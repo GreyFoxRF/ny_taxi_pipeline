08595966fed139bd2730d618a9229f2deeafec31
Fox <49479670+GreyFoxRF@users.noreply.github.com>
2026-04-05 10:54:28 -0700

    Remove Hadoop environment variable settings
    Removed HADOOP_HOME and PATH environment variable settings.

--- src/spark_session.py
+++ src/spark_session.py
@@ -9,10 +9,6 @@ logger = setup_logger()
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-# Указываем путь к нашей новой папке
-os.environ['HADOOP_HOME'] = r'C:\hadoop'
-# Добавляем её в системный путь, чтобы Spark нашел файлы
-os.environ['PATH'] = os.environ['HADOOP_HOME'] + r'\bin;' + os.environ.get('PATH', '')
 
 def create_spark_session():
     logger.info("--> Инициализация ядра PySpark...")
